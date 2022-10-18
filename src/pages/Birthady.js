4211fe9fd214186411b41570fb7fcf029f6b116d
Thor-miwa <adetomiw1@gmail.com>
2022-10-18 21:13:30 +0100

    removed bottom image

--- src/pages/Birthady.js
+++ src/pages/Birthady.js
@@ -1,14 +1,9 @@
 import Header from "../components/Header"
-import tomandjerry from "../assets/tomandjerry.jpg"
 
 function Birthady() {
   return (
     <div>
         <Header/>
-        <div>
-            <img src={tomandjerry} alt="tom"/>
-            <h1>Hello</h1>
-        </div>
     </div>
   )
 }
