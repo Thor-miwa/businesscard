e10f9483409c5ffdacd13206be95081d4734f709
Thor-miwa <adetomiw1@gmail.com>
2022-10-18 20:54:39 +0100

    removed last image

--- src/pages/Birthady.js
+++ src/pages/Birthady.js
@@ -5,10 +5,6 @@ function Birthady() {
   return (
     <div>
         <Header/>
-        <div>
-            <img src={tomandjerry} alt="tom"/>
-            <h1>Hello</h1>
-        </div>
     </div>
   )
 }
